3f96a4847252519bb1bf60cf0075ef899a9bfd60
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-08-19 19:46:02 +0530

    Create First Web Application

--- DummyProject/myapp/views.py
+++ DummyProject/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    html = '<h1> My first web application </h1>'
+    return HttpResponse(html)
