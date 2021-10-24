49a3a40a4bcb8dbed4fe76d2889a96a7287911ef
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-10-24 19:45:17 +0530

    Handling edge case in get request

--- GoogleForms/forms/utils.py
+++ GoogleForms/forms/utils.py
@@ -1,6 +1,8 @@
 import pymongo
 from django.utils import timezone
 
+from bson import ObjectId
+
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['GoogleForms']
 col = db['GoogleForms']
@@ -11,3 +13,14 @@ class Form:
             'created':timezone.now()
         })
         return str(obj.inserted_id)
+
+    def find(self, pk):
+        query = {
+            '_id':ObjectId(pk)
+        }
+        print(col.find(query))
+        print(type(col.find(query)))
+
+        obj = col.find(query).next() # cursor
+
+        return obj
--- GoogleForms/forms/views.py
+++ GoogleForms/forms/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.http import Http404
 from .utils import Form
 
 # Create your views here.
@@ -17,4 +18,12 @@ def create(request):
     return HttpResponseRedirect(reverse_lazy('forms:update_form', args=[pk]))
 
 def update_form(request, pk):
-    return render(request, 'update_form.html')
+    try:
+        form_data = form.find(pk)
+        return render(request, 'update_form.html', {
+            'pk':pk,
+            'form':form_data
+        })
+    except Exception as e:
+        print(e)
+        raise Http404('Form not found')
