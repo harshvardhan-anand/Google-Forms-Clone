c3b099dcf11ddb0f44cdc36a2747a3ad6e34fa01
Harshvardhan Anand <50915996+harshvardhan-anand@users.noreply.github.com>
2021-10-24 18:28:18 +0530

    Created baisc logic for Create New form operation

--- GoogleForms/forms/urls.py
+++ GoogleForms/forms/urls.py
@@ -5,4 +5,6 @@ app_name = 'forms'
 
 urlpatterns = [
     path('', views.home,name = 'home',),
+    path('create', views.create, name='create'),
+    path('forms/<str:pk>/edit', views.update_form, name='update_form'),
 ]
--- GoogleForms/forms/views.py
+++ GoogleForms/forms/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+def get_random_uuid():
+    return 'my_simple_uuid'
+
 def home(request):
     return render(request, 'home.html')
+
+def create(request):
+    pk = get_random_uuid()
+    return HttpResponseRedirect(reverse_lazy('forms:update_form', args=[pk]))
+
+def update_form(request, pk):
+    return render(request, 'update_form.html')
